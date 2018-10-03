7eaeecb8378f3560aa5c86085a8e33358f125edc
Jerry <Jerryyuanll@gmail.com>
2018-10-03 13:07:45 +0800

    downloadimage

---
+++ Downloader2.py
@@ -0,0 +1,11 @@
+import urllib.request
+import random
+
+
+def downloader(image_url):
+    file_name = random.randrange(1,10000)
+    full_file_name = str(file_name) + '.jpg'
+    urllib.request.urlretrieve(image_url,full_file_name)
+
+url = 'https://www.metalgearinformer.com/wp-content/uploads/2018/07/Metal-Gear-Survive-The-Encounter-2014-1.jpg'
+downloader(url)
