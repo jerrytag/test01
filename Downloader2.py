84c7a370280b7f6471c4e1756829e11dde83dd66
Jerry <Jerryyuanll@gmail.com>
2018-10-03 13:35:59 +0800

    downloadimage2

--- Downloader2.py
+++ Downloader2.py
@@ -7,5 +7,5 @@ def downloader(image_url):
     full_file_name = str(file_name) + '.jpg'
     urllib.request.urlretrieve(image_url,full_file_name)
 
-url = 'https://www.metalgearinformer.com/wp-content/uploads/2018/07/Metal-Gear-Survive-The-Encounter-2014-1.jpg'
+url = 'https://www.metalgearinformer.com/wp-content/uploads/2018/07/Metal-Gear-Survive-The-Encounter-2014-2.jpg'
 downloader(url)
